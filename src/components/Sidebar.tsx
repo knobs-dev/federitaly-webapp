5fbea1e8cad383d4bb95b4e22cc14bc4703f3479
matteo-poli <matteo.poli@knobs.it>
2023-09-14 17:08:45 +0200

    fix: logos

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -114,45 +114,45 @@ const Sidebar = () => {
             </li>
           </ul>
         </nav>
+        <div className="z-2 py-5 absolute bottom-0 left-5 right-5">
+          <h2 className="px-2 text-xs font-bold uppercase">
+            {clientT.value?.Common.sidebar.partner}
+          </h2>
+          <hr className="mt-1 border-[#D1D3D499]" />
+          <ul className="mt-5 flex items-center justify-between px-2">
+            <li>
+              <a href="https://dfinity.org/" target="_blank" rel="noreferrer">
+                <Image
+                  width={69}
+                  height={36}
+                  src="/assets/images/dfinity-logo-white.png"
+                  alt="DFINITY logo"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="https://knobs.it/" target="_blank" rel="noreferrer">
+                <Image
+                  width={73}
+                  height={26}
+                  src="/assets/images/knobs-logo-white.png"
+                  alt="KNOBS logo"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.origyn.com" target="_blank" rel="noreferrer">
+                <Image
+                  width={97}
+                  height={22}
+                  src="/assets/images/origyn-logo-white.png"
+                  alt="ORIGYN logo"
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
       </main>
-      <footer className="z-2 px-8 py-5">
-        <h2 className="px-2 text-xs font-bold uppercase">
-          {clientT.value?.Common.sidebar.partner}
-        </h2>
-        <hr className="mt-1 border-[#D1D3D499]" />
-        <ul className="mt-5 flex items-center justify-between px-2">
-          <li>
-            <a href="https://dfinity.org/" target="_blank" rel="noreferrer">
-              <Image
-                width={69}
-                height={36}
-                src="/assets/images/dfinity-logo-white.png"
-                alt="DFINITY logo"
-              />
-            </a>
-          </li>
-          <li>
-            <a href="https://knobs.it/" target="_blank" rel="noreferrer">
-              <Image
-                width={73}
-                height={26}
-                src="/assets/images/knobs-logo-white.png"
-                alt="KNOBS logo"
-              />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.origyn.com" target="_blank" rel="noreferrer">
-              <Image
-                width={97}
-                height={22}
-                src="/assets/images/origyn-logo-white.png"
-                alt="ORIGYN logo"
-              />
-            </a>
-          </li>
-        </ul>
-      </footer>
     </motion.aside>
   );
 };
