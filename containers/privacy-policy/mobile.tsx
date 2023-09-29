b3e5606e0cad6f2d7e1372917ab404d946c9e40b
matteo-poli <matteo.poli@knobs.it>
2023-09-29 12:22:25 +0200

    feat: adds privacy policy desktop page

---
+++ containers/privacy-policy/mobile.tsx
@@ -0,0 +1,17 @@
+import { useTranslations } from "@hooks/useTranslations";
+
+const PrivacyPolicyMobile = () => {
+  const t = useTranslations("PrivacyPolicy");
+
+  return (
+    <div className="flex flex-col space-y-4">
+      <h2 className="text-[0.9375rem] font-bold">{t("content.0")}</h2>
+      <h2 className="text-[0.9375rem] font-bold">{t("content.1")}</h2>
+      <p className="text-[0.9375rem] font-normal">{t("content.2")}</p>
+      <h2 className="text-[0.9375rem] font-bold">{t("content.3")}</h2>
+      <p className="text-[0.9375rem] font-normal">{t("content.4")}</p>
+    </div>
+  );
+};
+
+export default PrivacyPolicyMobile;
